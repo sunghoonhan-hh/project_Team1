6860258c82c1d270f8d5c85a2212fa3d8de6cf45
sunghoonhan-hh <hansoung2002@naver.com>
2025-06-17 13:01:06 +0900

    dk

--- Kiosk/Kiosk/Form_simple_payment.cs
+++ Kiosk/Kiosk/Form_simple_payment.cs
@@ -16,5 +16,10 @@ namespace Kiosk
         {
             InitializeComponent();
         }
+
+        private void txtCuponInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
